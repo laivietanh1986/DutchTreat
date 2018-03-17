5f8b9bc79d93a78770a4fb5fa21d270ebf316922
lai viet anh <laivietanh1986@gmail.com>
2018-03-17 23:04:00 +0700

    update code for mvc

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -15,6 +15,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,18 @@ namespace DutchTreat
             {
                 app.UseDeveloperExceptionPage();
             }
+            
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "index" });
+
             });
+          
         }
     }
 }
